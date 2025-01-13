5a7f885a8962f7cf8dab7d2073a7499f1f474b92
florence <loracodes3@gmail.com>
2025-01-13 20:20:01 +0300

    commit

---
+++ index.py
@@ -0,0 +1,12 @@
+def school_age_calculator (age, name):
+    if age < 5:
+        print(f"Enjoy more cartoon at home {name} is only {age}")
+    elif age == 5:
+        print ("Enjoy Kindergarten")
+
+    else:
+        print("Thet grow up so fast")
+
+school_age_calculator(3, "Florence")
+        
+
